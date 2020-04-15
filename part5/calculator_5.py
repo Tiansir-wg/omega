e7447293d85b40ef44e27382dd392d8cafab49b9
tiansir <15872397013@163.com>
2020-04-15 13:04:44 +0800

    任意个多位数字的加减乘除混合运算,考虑了优先级和结合性

---
+++ part5/calculator_5.py
@@ -0,0 +1,166 @@
+# EOF token表示到达输入串尾部
+token_types = {
+    "INT": "INT",
+    "MUL": "MUL",
+    "EOF": "EOF",
+    "DIV": "DIV",
+    "PLUS": "PLUS",
+    "MINUS": "MINUS"
+}
+
+
+class Token(object):
+    def __init__(self, type_, value_):
+        # token的类型,如INT,PLUS,EOF
+        self.type = type_
+        # token的值,如1,2,+和None
+        self.value = value_
+
+    def __str__(self):
+        """返回token的字符串形式
+
+        比如:
+        Token(INTEGER, 3)
+        """
+        return 'Token({type}, {value})'.format(
+            self.type,
+            self.__repr__(self.value))
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Lexser(object):
+    def __init__(self, text):
+        # 输入,比如1+2
+        self.text = text
+        # 当前待处理字符在text的位置,初始时为0
+        self.pos = 0
+        # 当前待处理字符
+        self.currentChar = self.text[self.pos]
+
+    def error(self):
+        raise Exception("不合法的字符")
+
+    def advance(self):
+        self.pos += 1
+        if self.pos >= len(self.text):
+            self.currentChar = None
+        else:
+            self.currentChar = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.currentChar is not None and self.currentChar.isspace():
+            self.advance()
+
+    def get_int(self):
+        result = ''
+        while self.currentChar is not None and self.currentChar.isdigit():
+            result += self.currentChar
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.currentChar is not None:
+            # 空格
+            if self.currentChar.isspace():
+                self.skip_whitespace()
+                continue
+
+            # 数字
+            if self.currentChar.isdigit():
+                return Token(token_types["INT"], self.get_int())
+
+            # 乘号
+            if self.currentChar == '*':
+                self.advance()
+                return Token(token_types["MUL"], '*')
+
+            # 除号
+            if self.currentChar == '/':
+                self.advance()
+                return Token(token_types["DIV"], '/')
+
+            # 加法
+            if self.currentChar == '+':
+                self.advance()
+                return Token(token_types["PLUS"], '+')
+
+            # 减法
+            if self.currentChar == '-':
+                self.advance()
+                return Token(token_types["MINUS"], '-')
+
+            self.error()
+
+        return Token(token_types["EOF"], None)
+
+
+class Interpreter(object):
+    def __init__(self, lexser):
+        self.lexser = lexser
+        self.current_token = self.lexser.get_next_token()
+
+    def error(self):
+        raise Exception("不合法的标识")
+
+    def eat(self, type_):
+        if self.current_token.type == type_:
+            self.current_token = self.lexser.get_next_token()
+        else:
+            self.error()
+
+    # factor::= INTEGER
+    def factor(self):
+        token = self.current_token
+        self.eat(token_types["INT"])
+        return token.value
+
+    # term::=factor((MUL|DIV)factor)*
+    def term(self):
+        result = self.factor()
+        while self.current_token.type in (token_types["MUL"], token_types["DIV"]):
+            token = self.current_token
+            if token.type == token_types["MUL"]:
+                self.eat(token_types["MUL"])
+                result = result * self.factor()
+            elif token.type == token_types["DIV"]:
+                self.eat(token_types["DIV"])
+                result = result / self.factor()
+
+        return result
+
+    # expr::=term((PLUS|MINUS)term)*
+    def expr(self):
+        result = self.term()
+        while self.current_token.type in (token_types["PLUS"], token_types["MINUS"]):
+            token = self.current_token
+            if token.type == token_types["PLUS"]:
+                self.eat(token_types["PLUS"])
+                result = result + self.term()
+            elif token.type == token_types["MINUS"]:
+                self.eat(token_types["MINUS"])
+                result = result - self.term()
+
+        return result
+
+
+def main():
+    while True:
+        try:
+            text = input("calc> ")
+            if text == 'exit':
+                break
+        except IOError:
+            break
+        # 输入为空的情况
+        if not text:
+            continue
+        lexser = Lexser(text)
+        interceptor = Interpreter(lexser)
+        result = interceptor.expr()
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
